87e94e03cbf5312e4ee7c4807deac715a45282af
Milan Sacher <milan.lanlada@gmail.com>
2018-08-18 20:23:18 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,6 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.upcase
-    puts "NO, NOT SINCE 1983!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
   end
